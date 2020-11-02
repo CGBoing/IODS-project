70db15c8c4efecdcd68b0517065f5de06d79457b
Jopi Mikkonen <jopi.mikkonen@iki.fi>
2020-11-02 14:18:25 +0200

    Initial commit for exercise 2.

---
+++ data/create_learning2014.R
@@ -0,0 +1,14 @@
+# ==============================================================================
+# ___
+# 
+# Jopi J. W. Mikkonen
+# 5th November 2020
+# ==============================================================================
+
+lrn14 <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+# lrn14
+
+str(lrn14)
+dim(lrn14)
+
