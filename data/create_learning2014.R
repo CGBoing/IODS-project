7d64977ecc73a0c9dcecde893afd3e7242f1a9af
Jopi Mikkonen <jopi.mikkonen@iki.fi>
2020-11-04 15:07:16 +0200

    Improvements to wording and script comments.

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -1,5 +1,6 @@
 # ==============================================================================
-# ___
+# IODS 2020 exercise 2 data wrangling - regression and model validation
+# Data from https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt
 # 
 # Jopi J. W. Mikkonen
 # 5th November 2020
@@ -12,3 +13,7 @@ lrn14 <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-
 str(lrn14)
 dim(lrn14)
 
+# Alternative to the above, for testing only
+students14 <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/learning2014.txt", sep=",", header=TRUE)
+str(students14)
+dim(students14)
