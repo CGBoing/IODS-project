597e4e28d1443d27d70cfe1d798bec294cf36daf
Jopi Mikkonen <jopi.mikkonen@iki.fi>
2020-11-04 15:40:25 +0200

    Testing file operations.

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -6,14 +6,41 @@
 # 5th November 2020
 # ==============================================================================
 
+# ==============================================================================
+# HELPER VARIABLES
+# ==============================================================================
+
+PROJECT_DIR <- getwd()
+DATA_FOLDER <- paste(PROJECT_DIR, "/data", sep="")
+
+# ==============================================================================
+# MAIN SCRIPT
+# ==============================================================================
+
 lrn14 <- read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
 
-# lrn14
+# Take a look at the data
+lrn14
 
+# Show the structure of the data frame
 str(lrn14)
-dim(lrn14)
 
-# Alternative to the above, for testing only
-students14 <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/learning2014.txt", sep=",", header=TRUE)
-str(students14)
-dim(students14)
+# Show the dimensions of the data frame
+dim(lrn14) # Should be _ rows and _ columns
+
+setwd(DATA_FOLDER)
+
+write.csv(lrn14, file = "learning2014.csv")
+test <- read.csv("learning2014.csv")
+test
+str(test)
+
+setwd(PROJECT_DIR)
+
+# ==============================================================================
+# FOR TESTING ONLY
+# ==============================================================================
+
+# students14 <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/learning2014.txt", sep=",", header=TRUE)
+# str(students14)
+# dim(students14) # Should be 166 rows and 7 columns
